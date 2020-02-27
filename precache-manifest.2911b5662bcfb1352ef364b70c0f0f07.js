0059e2cd989c160f7ec95b6fd3d368dc71de16b5
acm13 <annacm@udel.edu>
2020-02-27 11:16:30 -0500

    Updates

---
+++ precache-manifest.2911b5662bcfb1352ef364b70c0f0f07.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f62476e2173baf24ca984f192af69be",
+    "url": "assets/background.jpg"
+  },
+  {
+    "revision": "a9f9c938bc67c1d77ace9dc24e8dbd7e",
+    "url": "assets/ball.png"
+  },
+  {
+    "revision": "ada1fbbdcf5af570cf455f3a459e43e5",
+    "url": "assets/broom.png"
+  },
+  {
+    "revision": "737488fc5f1211fe41f60940d6440c2d",
+    "url": "assets/citations.md"
+  },
+  {
+    "revision": "9d79a2ec01455afa082d23e42f457801",
+    "url": "assets/font/font.png"
+  },
+  {
+    "revision": "872f41e77eae3bfc4fba291b0145b12a",
+    "url": "assets/font/font.xml"
+  },
+  {
+    "revision": "93fbe9d93dd8920d0bb31022c696ecce",
+    "url": "assets/lose.png"
+  },
+  {
+    "revision": "22cd595a8e2d4e79145de466032ca881",
+    "url": "assets/opponent.png"
+  },
+  {
+    "revision": "3bf238a86d7730bcfbe4b7b78ae72b1b",
+    "url": "assets/win.png"
+  },
+  {
+    "revision": "dd6d3695a2f100ce72396d17f14a7f44",
+    "url": "index.html"
+  },
+  {
+    "revision": "d8f5a26e8df2c5cc1846",
+    "url": "main.8a9a06cfc029b9bb2c62.bundle.js"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "sw.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "vendors.a047ee0740c7d21714d9.bundle.js"
+  }
+]);
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.2951ada0bdc6e22c707e452cb29f4093.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.2911b5662bcfb1352ef364b70c0f0f07.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /**
  * You should only modify this, if you know what you are doing.
