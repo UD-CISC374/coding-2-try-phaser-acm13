79c6cbe5eb49b0be50c73c9eaf2c3e98899ee6dc
acm13 <annacm@udel.edu>
2020-02-27 11:13:14 -0500

    Updates

---
+++ dev/precache-manifest.a565e5e68ea4c07d507156e4bb730521.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f62476e2173baf24ca984f192af69be",
+    "url": "/coding-2-try-phaser-acm13/devassets/background.jpg"
+  },
+  {
+    "revision": "a9f9c938bc67c1d77ace9dc24e8dbd7e",
+    "url": "/coding-2-try-phaser-acm13/devassets/ball.png"
+  },
+  {
+    "revision": "ada1fbbdcf5af570cf455f3a459e43e5",
+    "url": "/coding-2-try-phaser-acm13/devassets/broom.png"
+  },
+  {
+    "revision": "737488fc5f1211fe41f60940d6440c2d",
+    "url": "/coding-2-try-phaser-acm13/devassets/citations.md"
+  },
+  {
+    "revision": "9d79a2ec01455afa082d23e42f457801",
+    "url": "/coding-2-try-phaser-acm13/devassets/font/font.png"
+  },
+  {
+    "revision": "872f41e77eae3bfc4fba291b0145b12a",
+    "url": "/coding-2-try-phaser-acm13/devassets/font/font.xml"
+  },
+  {
+    "revision": "93fbe9d93dd8920d0bb31022c696ecce",
+    "url": "/coding-2-try-phaser-acm13/devassets/lose.png"
+  },
+  {
+    "revision": "22cd595a8e2d4e79145de466032ca881",
+    "url": "/coding-2-try-phaser-acm13/devassets/opponent.png"
+  },
+  {
+    "revision": "3bf238a86d7730bcfbe4b7b78ae72b1b",
+    "url": "/coding-2-try-phaser-acm13/devassets/win.png"
+  },
+  {
+    "revision": "cfd4de19842dd5ccc99a",
+    "url": "/coding-2-try-phaser-acm13/devgen.main.a2d289312ff4563ec684.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/coding-2-try-phaser-acm13/devgen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "9d36ff8fe9b94a25f417be6796081bd1",
+    "url": "/coding-2-try-phaser-acm13/devindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/coding-2-try-phaser-acm13/devmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/coding-2-try-phaser-acm13/devsw.js"
+  }
+]);
--- dev/sw.js
+++ dev/sw.js
@@ -1,4 +1,4 @@
-importScripts("/coding-2-try-phaser-acm13/devprecache-manifest.01475a0fef125be7498d696e1479c31a.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/coding-2-try-phaser-acm13/devprecache-manifest.a565e5e68ea4c07d507156e4bb730521.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /**
  * You should only modify this, if you know what you are doing.
