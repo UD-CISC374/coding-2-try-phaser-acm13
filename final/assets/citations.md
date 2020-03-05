3d8db8edc5389013a9a6dbe79117ddea01f683e1
acm13 <annacm@udel.edu>
2020-03-04 22:21:44 -0500

    Updates

---
+++ final/assets/citations.md
@@ -0,0 +1,21 @@
+*Please be sure to list the source of all assets you used here.*
+
+*At least a URL, but also be sure to include any extra information that the artist/author requests you include.*
+
+Welcome to Broomball Training Camp! In this workshop you become one with the broom and you must steal the ball
+before your otter opponents get it.
+
+Sources:
+
+background.png:
+https://www.freepik.com/premium-photo/ice-background_5254410.htm#page=1&query=ice&position=11
+ball.png:
+http://www.clker.com/clipart-dark-blue-ball.html
+broom.png:
+https://pngimage.net/broom-cartoon-png-3/
+lose.png:
+https://www.clipart.email/clipart/big-red-x-clipart-24359.html
+opponent.png:
+https://webstockreview.net/explore/hockey-clipart-hockey-practice/
+win.png:
+https://www.iconfinder.com/icons/1739338/cartoon_coin_dollar_finance_gold_investment_money_icon
---
+++ final/precache-manifest.376dd186213bb984168a8e327d9e120f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f62476e2173baf24ca984f192af69be",
+    "url": "/coding-2-try-phaser-acm13/finalassets/background.jpg"
+  },
+  {
+    "revision": "963031d370e8c7286fa2ce46c923af87",
+    "url": "/coding-2-try-phaser-acm13/finalassets/ball.png"
+  },
+  {
+    "revision": "86e7b420e36fbd689ef47cc3b361d699",
+    "url": "/coding-2-try-phaser-acm13/finalassets/broom.png"
+  },
+  {
+    "revision": "737488fc5f1211fe41f60940d6440c2d",
+    "url": "/coding-2-try-phaser-acm13/finalassets/citations.md"
+  },
+  {
+    "revision": "9d79a2ec01455afa082d23e42f457801",
+    "url": "/coding-2-try-phaser-acm13/finalassets/font/font.png"
+  },
+  {
+    "revision": "872f41e77eae3bfc4fba291b0145b12a",
+    "url": "/coding-2-try-phaser-acm13/finalassets/font/font.xml"
+  },
+  {
+    "revision": "c177fd0d8dffbeb2ce04909a7b4b9b37",
+    "url": "/coding-2-try-phaser-acm13/finalassets/lose.png"
+  },
+  {
+    "revision": "41591c19bf847a5ad7b5417ad7724951",
+    "url": "/coding-2-try-phaser-acm13/finalassets/opponent.png"
+  },
+  {
+    "revision": "2149ade431b6b26afb7f5af226940608",
+    "url": "/coding-2-try-phaser-acm13/finalassets/win.png"
+  },
+  {
+    "revision": "054eda29918e4a9c7dfd",
+    "url": "/coding-2-try-phaser-acm13/finalgen.main.7aa873af40181d0d362e.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/coding-2-try-phaser-acm13/finalgen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "6c784f962caeecaae744a55a72b48a07",
+    "url": "/coding-2-try-phaser-acm13/finalindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/coding-2-try-phaser-acm13/finalmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/coding-2-try-phaser-acm13/finalsw.js"
+  }
+]);
